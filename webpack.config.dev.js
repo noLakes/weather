2ad0b809c2989facb58459a62fb68194e432c243
“noLakes” <51928001+noLakes@users.noreply.github.com>
2021-03-25 15:43:28 -0400

    finished v1 styling

---
+++ webpack.config.dev.js
@@ -0,0 +1,18 @@
+"use strict";
+
+var path = require('path');
+
+var Dotenv = require('dotenv-webpack');
+
+module.exports = {
+  mode: 'production',
+  entry: {
+    index: './src/index.js'
+  },
+  devtool: 'inline-source-map',
+  output: {
+    filename: 'main.js',
+    path: path.resolve(__dirname, 'dist')
+  },
+  plugins: [new Dotenv()]
+};
